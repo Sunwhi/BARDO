3800f834ca515568bcd9be0b341f5280fc3a9b3d
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-05-25 16:24:33 +0900

    feat : Player Base Script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public PlayerStateMachine fsm { get; private set; }
+    public PlayerController controller { get; private set; }
+
+    [Header("Components")]
+    public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    [Header("Animation")]
+    [SerializeField] private PlayerAnimationData animationData;
+    public PlayerAnimationData AnimationData => animationData;
+
+    private void Awake()
+    {
+        animationData.Initialize();
+        controller = new PlayerController(this);
+        fsm = new PlayerStateMachine(this);
+    }
+
+    private void Start()
+    {
+        fsm.ChangeState(fsm.IdleState);
+    }
+
+    private void Update()
+    {
+        fsm.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        fsm.FixedUpdate();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        controller.MoveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            controller.JumpInput = true;
+        }
+    }
+}
