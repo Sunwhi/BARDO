3a59d99f6f73f77bfdc8976341dd075c85f9654d
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-06-01 14:25:27 +0900

    feat : FadeView

---
+++ Assets/Scripts/UI/FadeView.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class Fadeview : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    
+    public IEnumerator FadeOut(float duration = 1f)
+    {
+        canvasGroup.DOFade(1f, duration);
+        yield return new WaitForSeconds(duration);
+    }
+    
+    public IEnumerator FadeIn(float duration = 1f)
+    {
+        canvasGroup.DOFade(0f, duration);
+        yield return new WaitForSeconds(duration);
+    }
+}
