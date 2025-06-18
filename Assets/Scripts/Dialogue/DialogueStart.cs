39e7c4bf9c75e877f399af8e72b817e3116a09f5
Sunwhi <80499477+Sunwhi@users.noreply.github.com>
2025-06-18 17:09:16 +0900

    대화창 UI 변경
    대화글씨 크기 키우고, 발화자 이름 표시

--- Assets/Scripts/Dialogue/DialogueStart.cs
+++ Assets/Scripts/Dialogue/DialogueStart.cs
@@ -14,6 +14,7 @@ public class DialogueStart : MonoBehaviour
     }
     private void OnDisable()
     {
+        Debug.Log("OnDiable");
         GameEventManager.Instance.inputEvents.onSubmitPressed -= SubmitPressed;
     }
 
