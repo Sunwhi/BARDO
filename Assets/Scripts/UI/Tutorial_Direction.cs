1ae1a31c01e5b053cb807ae69b74a98d923d892d
Sunwhi <sunwhi123@naver.com>
2025-06-30 15:07:16 +0900

    튜토리얼 이미지 구현
    방향키랑 스페이스 바

--- Assets/Scripts/UI/Tutorial_Direction.cs
+++ Assets/Scripts/UI/Tutorial_Direction.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Collections.Generic;
 
 public class Tutorial : MonoBehaviour
 {
-    public Image directionUp;
-    public Image directionLeft;
-    public Image directionDown;
-    public Image directionRight;
+    private SpriteRenderer directionSprite;
 
+    private void Start()
+    {
+        directionSprite = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        GameEventBus.Subscribe<TutorialEvent>(Show);
+    }
 
+    private void OnDisable()
+    {
+        GameEventBus.Unsubscribe<TutorialEvent>(Show);
+    }
 
+    private void Show(TutorialEvent ev)
+    {
+        switch(ev.tutorialId)
+        {
+            case "Move_On":
+                directionSprite.DOFade(1f, 1f);
+                break;
+            case "Move_Off":
+                directionSprite.DOFade(0f, 1f);
+                break;  
+        }
+    }
 }
--- Assets/Scripts/UI/Tutorial_Spacebar.cs
+++ Assets/Scripts/UI/Tutorial_Spacebar.cs
@@ -1,16 +1,20 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class TutorialGuide : MonoBehaviour
+public class Tutorial_Spacebar : MonoBehaviour
 {
-    public Image image;
+    private SpriteRenderer spaceBar;
+
+    private void Start()
+    {
+        spaceBar = GetComponent<SpriteRenderer>();
+    }
 
     private void OnEnable()
     {
         GameEventBus.Subscribe<TutorialEvent>(Show);
     }
-
     private void OnDisable()
     {
         GameEventBus.Unsubscribe<TutorialEvent>(Show);
@@ -20,11 +24,11 @@ public class TutorialGuide : MonoBehaviour
     {
         switch (ev.tutorialId)
         {
-            case "Move_On":
-                image.DOFade(1f, 1f);
+            case "Jump_On":
+                spaceBar.DOFade(1f, 1f);
                 break;
-            case "Move_Off":
-                image.DOFade(0f, 1f);
+            case "Jump_Off":
+                spaceBar.DOFade(0f, 1f);
                 break;
         }
     }
