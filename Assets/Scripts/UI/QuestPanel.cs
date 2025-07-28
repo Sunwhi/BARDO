823b371cd6424a9e95cf93e8f8dad72a42196b8b
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-07-28 16:03:47 +0900

    feat : QuestPanel에 이벤트 구독해서 SaveData에 따른 UI 자동반영

--- Assets/Scripts/UI/QuestPanel.cs
+++ Assets/Scripts/UI/QuestPanel.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -6,11 +7,27 @@ public class QuestPanel : UIBase
 {
     [SerializeField] private TextMeshProUGUI titleTxt;
     [SerializeField] private RectTransform contentParent;
+    [SerializeField] private GameObject subQuestTxtPrefab;
+    private List<TextMeshProUGUI> subQuestTxts;
 
     private bool isSlided = false;
     private bool isOpened = true;
     private Tween slideTween;
     private Tween openTween;
+
+    private QuestData currentQuestData;
+
+    private void OnEnable()
+    {
+        GameEventBus.Subscribe<DataChangeEvent<QuestData>>(OnQuestDataChanged);
+        currentQuestData = SaveManager.Instance.MySaveData.currentQuest;
+        SetQuestData();
+    }
+
+    private void OnDisable()
+    {
+        GameEventBus.Unsubscribe<DataChangeEvent<QuestData>>(OnQuestDataChanged);
+    }
 
     public void SetSlide()
     {
@@ -36,5 +53,31 @@ public class QuestPanel : UIBase
             .OnComplete(() => openTween = null);
 
         isOpened = !isOpened;
+    }
+
+    private void OnQuestDataChanged(DataChangeEvent<QuestData> @event)
+    {
+        currentQuestData = @event.NewValue;
+        SetQuestData();
+    }
+
+    private void SetQuestData()
+    {
+        if (currentQuestData == null)
+        {
+            titleTxt.text = "No Active Quest";
+            return;
+        }
+
+        titleTxt.text = currentQuestData.QuestTitle;
+        for (int i = 0; i < currentQuestData.SubQuests.Length; i++)
+        {
+            var subQuest = currentQuestData.SubQuests[i];
+            subQuestTxts.Add(
+                Instantiate(subQuestTxtPrefab.gameObject, contentParent)
+                .GetComponent<TextMeshProUGUI>()
+                );
+            subQuestTxts[i].text = $"({subQuest.SubQuestID}) {subQuest.SubQuestName}";
+        }
     }
 }
