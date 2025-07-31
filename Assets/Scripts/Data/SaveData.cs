72b87913d0680b8e09abc20872ee00859a4c7e18
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-07-31 18:13:52 +0900

    update : add SaveData

--- Assets/Scripts/Data/SaveData.cs
+++ Assets/Scripts/Data/SaveData.cs
@@ -19,6 +19,9 @@ public class SaveData
     public int stageIdx = 0; //0 : 시작. 1~ : N주차.
     public int storyIdx = 0; //Stage 내부 Idx
 
+    public bool isQuestActive = false;
+    public QuestData currentQuest = null;
+
     public bool firstJudgement = true; //true = 선. false = 악.
     public bool secondJudgement = true; //true = 선. false = 악.
     public EEnding ending = EEnding.Enlightenment; // Ending.
