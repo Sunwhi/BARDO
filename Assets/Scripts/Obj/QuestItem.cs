4a60d5a3738ab0fe9376b723de056b75ed04fbcf
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-08-13 12:13:36 +0900

    update : MainScene&QuestItem 획득시 아이템 획득 UI 띄우기

--- Assets/Scripts/Obj/QuestItem.cs
+++ Assets/Scripts/Obj/QuestItem.cs
@@ -20,12 +20,15 @@ public class QuestItem : MonoBehaviour
             {
                 case eQuestItems.Karmic_Shard:
                     QuestManager.Instance.ClearSubQuest(0);
+                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(nameof(ItemDetailPanel), new object[] { eItemPanelType.Karmic_Shard });
                     break;
                 case eQuestItems.Memory_Lamp:
                     QuestManager.Instance.ClearSubQuest(1);
+                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(nameof(ItemDetailPanel), new object[] { eItemPanelType.Memory_Lamp });
                     break;
                 case eQuestItems.Soul_Thread:
                     QuestManager.Instance.ClearSubQuest(2);
+                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(nameof(ItemDetailPanel), new object[] { eItemPanelType.Soul_Thread });
                     break;
             }
         }
