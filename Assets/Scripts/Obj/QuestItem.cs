e6be63a4be52f6339cb8bac43f9d7c84d458bff1
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-08-20 12:36:52 +0900

    update : QuestItem 획득 후 저장 시 게임 재실행해도 아이템이 떠오르지 않도록 수정

--- Assets/Scripts/Obj/QuestItem.cs
+++ Assets/Scripts/Obj/QuestItem.cs
@@ -12,25 +12,48 @@ public class QuestItem : MonoBehaviour
 {
     [SerializeField] private eQuestItems itemType;
 
+    private void Start()
+    {
+        SaveData saveData = SaveManager.Instance.MySaveData;
+        gameObject.SetActive(!saveData.questItemAcquired[(int)itemType]);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            SaveManager.Instance.SetSaveData(nameof(SaveData.questItemAcquired), true, (int)itemType);
+
             switch (itemType)
             {
                 case eQuestItems.Karmic_Shard:
                     QuestManager.Instance.ClearSubQuest(0);
-                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(nameof(ItemDetailPanel), new object[] { eItemPanelType.Karmic_Shard });
+                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(eItemPanelType.Karmic_Shard);
                     break;
                 case eQuestItems.Memory_Lamp:
                     QuestManager.Instance.ClearSubQuest(1);
-                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(nameof(ItemDetailPanel), new object[] { eItemPanelType.Memory_Lamp });
+                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(eItemPanelType.Memory_Lamp);
                     break;
                 case eQuestItems.Soul_Thread:
                     QuestManager.Instance.ClearSubQuest(2);
-                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(nameof(ItemDetailPanel), new object[] { eItemPanelType.Soul_Thread });
+                    UIManager.Instance.ShowPanelWithParam<ItemDetailPanel>(eItemPanelType.Soul_Thread);
                     break;
             }
+        }
+
+        bool isAllItemsAcquired = true;
+        foreach (var item in SaveManager.Instance.MySaveData.questItemAcquired)
+        {
+            if (!item)
+            {
+                isAllItemsAcquired = false;
+                break;
+            }
+        }
+
+        if (isAllItemsAcquired)
+        {
+
         }
 
         gameObject.SetActive(false);
