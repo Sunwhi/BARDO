e6c6c6b098bb2c96f81ebe5cf3c78817fde4844e
Sunwhi <80499477+Sunwhi@users.noreply.github.com>
2025-09-03 12:45:10 +0900

    save이름 지정 저장 구현

--- Assets/Scripts/Data/SaveData.cs
+++ Assets/Scripts/Data/SaveData.cs
@@ -12,8 +12,8 @@ public enum EEnding
 [Serializable]
 public class SaveData
 {
-    public string slotNumber = "[슬롯0]";
-    public string saveName = "New Save";
+    public string saveName = "[슬롯0]";
+    public string stageName = "New Save";
     public string checkPointName = "empty";
     public long lastSaveTime = 0;
     public bool dataSaved = false;
