73b37ea6482a94f5df5bfc82d73cdc087ccddb87
Sunwhi <sunwhi123@naver.com>
2025-09-30 02:12:11 +0900

    titlescene 버튼 수정

--- Assets/Scripts/UI/Defaults/ButtonHoverEffect.cs
+++ Assets/Scripts/UI/Defaults/ButtonHoverEffect.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler   
 {
-    [SerializeField] GameObject normalStateObject;
     [SerializeField] GameObject hoverStateObject;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Debug.Log("aoieisojf");
         SoundManager.Instance.PlaySFX(ESFX.UI_Button_Hover);
-        if(normalStateObject != null) normalStateObject.SetActive(false);
         if(hoverStateObject != null) hoverStateObject.SetActive(true);
     }
     public void OnPointerExit(PointerEventData eventData) 
     {
-        if (normalStateObject != null) normalStateObject.SetActive(true);
         if (hoverStateObject != null) hoverStateObject.SetActive(false);
     }
 }
