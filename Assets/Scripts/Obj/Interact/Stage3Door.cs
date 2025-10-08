546e5118d42334686506366a1f643a87c50fb7a2
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-10-08 18:49:53 +0900

    fix : 자가참조 하던 코드 수정 - stage3의 door 상호작용 정상화

--- Assets/Scripts/Obj/Interact/Stage3Door.cs
+++ Assets/Scripts/Obj/Interact/Stage3Door.cs
@@ -3,18 +3,13 @@ using UnityEngine;
 
 public class Stage3Door : InteractEnter
 {
-    //{stage , story}
-    private readonly int[] validStoryIndices = { 3, 4 };
-
     protected override IEnumerator InteractCoroutine()
     {
         yield return base.InteractCoroutine();
 
-        if (stageIdx == validStoryIndices[0] && stageIdx == validStoryIndices[1])
+        if (stageIdx == SaveManager.Instance.MySaveData.stageIdx && stageIdx == SaveManager.Instance.MySaveData.storyIdx)
         {
             UIManager.Show<CardPanel>();
         }
-
-        yield return null;
     }
 }
