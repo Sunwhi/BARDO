b15a935f3a114231401b270bfd01afe7e4a8cf5f
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-10-08 21:39:45 +0900

    feat : stage4로 임시 연결되는 코드

--- Assets/Scripts/Obj/Interact/Stage3Door.cs
+++ Assets/Scripts/Obj/Interact/Stage3Door.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class Stage3Door : InteractEnter
 {
+    [SerializeField] private Transform stage4PlayerPos;
+
     protected override IEnumerator InteractCoroutine()
     {
         yield return base.InteractCoroutine();
 
         if (stageIdx == SaveManager.Instance.MySaveData.stageIdx && stageIdx == SaveManager.Instance.MySaveData.storyIdx)
         {
-            UIManager.Show<CardPanel>();
+            UIManager.Show<CardPanel>(stage4PlayerPos.position);
         }
     }
 }
