7a06507f9aa75e4a6f8a3b0ef0976cbae427c7d8
Sunwhi <sunwhi123@naver.com>
2025-11-29 17:20:22 +0900

    제단 비디오 추가, sfx 수정
    sfc 플레이어 분리함.
    round transition 애니메이션 약간 수정

--- Assets/Scripts/UI/Popups/MapHintPanel.cs
+++ Assets/Scripts/UI/Popups/MapHintPanel.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class MapHintPanel : UIBase
 {
-
+    public override void OnUICloseBtn()
+    {
+        SoundManager.Instance.PlaySFX(ESFX.UI_Mouse_Click);
+        base.OnUICloseBtn();
+    }
 }
--- Assets/Scripts/UI/VideoPlayer/VideoController.cs
+++ Assets/Scripts/UI/VideoPlayer/VideoController.cs
@@ -1,19 +1,42 @@
 using UnityEngine;
 using UnityEngine.Video;
 using System;
+using System.Collections.Generic;
 
+public enum VideoType
+{
+    Intro,
+    Stage3,
+    Ending
+}
 public class VideoController : MonoBehaviour
 {
+    [Serializable]
+    public struct VideoMapping
+    {
+        public VideoType Type;
+        public VideoClip clip;
+    }
+
+    [Header("Components")]
     [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private GameObject videoDisplayUI;
     [SerializeField] private GameObject skipBtn;
+    [SerializeField] private GameObject blackPanel;
 
-    public event Action OnVideoFinished;
+    [Header("Video Library")]
+    [SerializeField] private List<VideoMapping> videoList;
+
+    public event Action<VideoType> OnVideoFinished;
+    private VideoType curVideoType;
+    private EBGM stoppedBgm; // 비디오 재생으로 멈춰진 현재 bgm 저장
 
     private void OnEnable()
     {
         if(videoPlayer != null)
             videoPlayer.loopPointReached += HandleVIdeoFinished;
+
+        blackPanel.SetActive(false); // continue시에 나타나지 않게
     }
     private void OnDisable()
     {
@@ -23,14 +46,37 @@ public class VideoController : MonoBehaviour
     private void HandleVIdeoFinished(VideoPlayer vp)
     {
         StopVideo();
-        Destroy(videoDisplayUI);
-        skipBtn.SetActive(false);
-        OnVideoFinished?.Invoke();
-    }
-    public void PlayVideo()
-    {
-        if (videoPlayer != null)
+
+        if(stoppedBgm != EBGM.Title) // 타이틀만 제외하고 이어서 재생
         {
+            SoundManager.Instance.PlayBGM(stoppedBgm);
+        }
+
+        videoDisplayUI.SetActive(false);
+        skipBtn.SetActive(false);
+        if (blackPanel.activeSelf)
+        {
+            blackPanel.SetActive(false);
+        }
+  
+
+        OnVideoFinished?.Invoke(curVideoType);
+    }
+    public void PlayVideo(VideoType type)
+    {
+        curVideoType = type;
+        blackPanel.SetActive(true); //video 시작하면 blackPanel 뒤로 깐다.
+        SoundManager.Instance.StopBGM();
+        stoppedBgm = SoundManager.Instance.GetCurrentBGM();
+
+        VideoMapping target = videoList.Find(x => x.Type == type);
+
+        if(target.clip != null && videoPlayer != null)
+        {
+            videoPlayer.clip = target.clip;
+            videoDisplayUI.SetActive(true);
+            skipBtn?.SetActive(true);
+
             videoPlayer.Play();
         }
     }
