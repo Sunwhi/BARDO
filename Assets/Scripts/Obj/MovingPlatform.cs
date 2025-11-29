a1b3fd52bce5f1fc2a2c2204da7e9ce9284af6f4
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-11-29 12:55:01 +0900

    update : moving platform pause 기능

--- Assets/Scripts/Obj/MovingPlatform.cs
+++ Assets/Scripts/Obj/MovingPlatform.cs
@@ -10,6 +10,9 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] private Transform endPoint;
     [SerializeField] private Transform platform;
 
+    [Header("Move Setting")]
+    [SerializeField] private bool isPauseAtEnd = false;
+
     Coroutine moveCoroutine;
 
     private void Update()
@@ -38,9 +41,16 @@ public class MovingPlatform : MonoBehaviour
             if (Vector3.Distance(platform.position, target) < 0.01f)
             {
                 target = (target == endPoint.position) ? startPoint.position : endPoint.position;
-            }
 
-            yield return null;
+                if (isPauseAtEnd)
+                {
+                    yield return new WaitForSeconds(1f);
+                }
+            }
+            else
+            {
+                yield return null;
+            }
         }
     }
 
